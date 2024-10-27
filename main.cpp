2cbde653413777e3d46b63b402bb808f8a2fb690
Abe <abenanduri1@gmail.com>
2024-10-26 22:44:37 -0700

    10th commit

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main() {
         trip.push_back(temp); 
     }
 
-    
+    display_trip(trip);    
 
 
     return 0;
@@ -89,8 +89,13 @@ int main_menu()
 }
 
 void display_trip(list<Goat> trip)
-{
-    for(auto it = trip.begin(); it!)
-    {}
+{   
+    int i = 1;
+    for(auto it = trip.begin(); it != trip.end(); ++it)
+    {
+        cout<<"["<<i<<"] "<<it->get_name()<<" "<<"( "<<it->get_age()<<", "
+        <<it->get_color()<<")"<<endl;
+        i++;
+    }
 }
 
